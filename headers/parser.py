f8b29f72e2a576f022e481e3f65c865be0154f56
Michal Melewski <michal.melewski@gmail.com>
2017-02-22 03:14:05 +0100

    New parser for function prototypes

--- headers/parser.py
+++ headers/parser.py
@@ -1,31 +1,65 @@
 #!/usr/bin/env python
 
+import glob
 import json
+import os
 import re
 import sys
 
+def parse(function_array):
+  fn_pattern = re.compile('(\w+)$')
+  functions = {}
+
+  for line in function_array:
+    if line.strip() == "" or line.startswith('//'):
+      continue
+
+    name, args = line.split('(',1)
+    f_name = fn_pattern.search(name).group(0)
+
+    if functions.has_key(f_name):
+      print "Name conflict for function: %s"%f_name
+      print functions[f_name]
+      print line
+      continue
+
+    functions[f_name] = []
+
+    for i, arg in enumerate(args.split(',')):
+      functions[f_name].append(arg.strip().rstrip(');'))
+
+  return json.dumps(functions, indent=2)
+
+def parse_file(file_name):
+  with open(file_name, "r") as fh:
+    return fh.readlines()
+
+def parse_dir(dir_name):
+    dir_list = []
+
+    for file_name in glob.glob(dir_name + '/*.prot'):
+      with open(file_name, "r") as fh:
+        dir_list.extend(fh.readlines())
+
+    return dir_list
+
 def main():
+  function_list = []
+
   if len(sys.argv) < 2:
     print("No filename provided")
     return -1
 
-  fn_pattern = re.compile('(\w+)$')
-  functions = {}
+  # Fetch content of all files in question
+  for name in sys.argv[1:]:
+    if os.path.isfile(name):
+      function_list.extend(parse_file(name))
 
-  with open(sys.argv[1], 'r') as fh:
-    for line in fh.readlines():
-      if line.strip() == "" or line.startswith('//'):
-        continue
+    if os.path.isdir(name):
+      function_list.extend(parse_dir(name))
 
-      name, args = line.split('(')
-      f_name = fn_pattern.search(name).group(0)
-      functions[f_name] = []
-
-      for i, arg in enumerate(args.split(',')):
-        functions[f_name].append(arg.strip().rstrip(');'))
-
-
-    print json.dumps(functions, indent=2)
+  # Parse it
+  print parse(function_list)
 
 if __name__ == "__main__":
   sys.exit(main())
