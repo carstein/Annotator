7ea3fc9a1775e516638a53567bfdcf51932e55f2
Michal Melewski <michal.melewski@gmail.com>
2018-03-28 00:36:14 +0200

    Changing format of function annotation

--- modules/annotate.py
+++ modules/annotate.py
@@ -13,6 +13,7 @@ call_llil = [
   LowLevelILOperation.LLIL_CALL,
   LowLevelILOperation.LLIL_CALL_STACK_ADJUST,
 ]
+
 # Simple database loader - assume all is in one file for now
 def load_database(data_path):
     fh = open(PLUGINDIR_PATH + '/data/' + data_path, 'r')
@@ -44,14 +45,14 @@ def run_plugin(bv, function):
       if (instruction.operation in call_llil and
           instruction.dest.operation == LowLevelILOperation.LLIL_CONST_PTR):
         callee = bv.get_function_at(instruction.dest.constant) # Fetching function in question
-        print callee
 
-        if (callee.symbol.type.name == 'ImportedFunctionSymbol' and db.has_key(callee.name)):
+        if (callee.symbol.type == SymbolType.ImportedFunctionSymbol and db.has_key(callee.name)):
           stack_args = iter(stack)
 
-          for function_arg in db[callee.name]:
+          for idx, function_arg in enumerate(db[callee.name]):
             try:
               stack_instruction = stack_args.next()
-              function.set_comment(stack_instruction.address, function_arg)
+              comment = "<arg{}: {}>\n".format(idx+1, function_arg)
+              function.set_comment(stack_instruction.address, comment)
             except StopIteration:
               log_error('[x] Virtual Stack Empty. Unable to find function arguments for <{}>'.format(callee.name))
